55629ff4304459da1700ed2f34a71d91cd77dc38
Lorenzo <lorenzofumi@gmail.com>
2025-10-14 12:47:26 +0200

    feat: added scrollarea to scroll pages

--- src/diary/ui/main_window.py
+++ src/diary/ui/main_window.py
@@ -1,4 +1,9 @@
-from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QScrollArea,
+    QWidget,
+    QVBoxLayout,
+)
 from PyQt6.QtCore import Qt
 
 from diary.ui.widgets.page_widget import PageWidget
@@ -12,7 +17,13 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("background-color: #2C2C2C;")
 
         central_widget = QWidget()
-        self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
-
         layout.addWidget(PageWidget(), alignment=Qt.AlignmentFlag.AlignCenter)
+
+        scroll_widget = QScrollArea()
+        scroll_widget.setWidget(central_widget)
+        scroll_widget.setWidgetResizable(False)
+        scroll_widget.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        scroll_widget.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        scroll_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(scroll_widget)

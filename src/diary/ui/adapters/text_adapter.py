3809a78e9374c9394ab25a1868e1e2456b281959
Lorenzo <lorenzofumi@gmail.com>
2025-10-24 10:08:19 +0200

    fix: text adapter

--- src/diary/ui/adapters/text_adapter.py
+++ src/diary/ui/adapters/text_adapter.py
@@ -1,8 +1,8 @@
 """Adapter for rendering Text elements with QPainter"""
 
 from typing import override
-from PyQt6.QtGui import QPainter, QPixmap, QBrush, QColor, QPen
-from PyQt6.QtCore import QRectF, Qt, QPointF
+from PyQt6.QtGui import QFont, QPainter
+from PyQt6.QtCore import QPointF
 
 from diary.models import PageElement, Text
 from diary.ui.adapters import ElementAdapter
@@ -23,44 +23,6 @@ class TextAdapter(ElementAdapter):
             return
 
         painter.save()
+        painter.setFont(QFont(family="Times New Roman", pointSize=int(element.size_px)))
         painter.drawText(QPointF(element.position.x, element.position.y), element.text)
         painter.restore()
-
-        # # Create the rectangle where the image will be drawn
-        # rect = QRectF(image.position.x, image.position.y, image.width, image.height)
-        # # Save the current painter state
-        # painter.save()
-        # # Apply rotation if needed
-        # if image.rotation != 0.0:
-        #     center = rect.center()
-        #     painter.translate(center)
-        #     painter.rotate(image.rotation)
-        #     painter.translate(-center)
-        # # Try to load and draw the actual image
-        # pixmap = None
-        # if image.image_path:
-        #     pixmap = QPixmap(image.image_path)
-        # elif image.image_data:
-        #     pixmap = QPixmap()
-        #     _ = pixmap.loadFromData(image.image_data)
-
-        # if pixmap and not pixmap.isNull():
-        #     # Draw the actual image
-        #     painter.drawPixmap(rect, pixmap, pixmap.rect().toRectF())
-        # else:
-        #     # Draw a placeholder rectangle if image can't be loaded
-        #     painter.setBrush(QBrush(QColor(200, 200, 200, 128)))
-        #     painter.setPen(QPen(QColor(100, 100, 100), 2, Qt.PenStyle.DashLine))
-        #     painter.drawRect(rect)
-
-        #     # Draw an "X" to indicate missing image
-        #     painter.setPen(QPen(QColor(150, 150, 150), 1))
-        #     painter.drawLine(rect.topLeft(), rect.bottomRight())
-        #     painter.drawLine(rect.topRight(), rect.bottomLeft())
-
-        #     # Draw "IMG" text in the center
-        #     painter.setPen(QPen(QColor(100, 100, 100)))
-        #     _ = painter.drawText(rect, Qt.AlignmentFlag.AlignCenter, "IMG")
-
-        # # Restore the painter state
-        # painter.restore()
